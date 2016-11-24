6d70fd6f5f9aa132428a4f38a8bf33ecbd408461
wzj <317619554@qq.com>
2016-11-24 17:31:59 +0800

    无

---
+++ weiguanjia/Application/Home/Controller/BasisController.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ji
+ * Date: 2016/11/24
+ * Time: 16:32
+ */
+
+namespace Home\Controller;
+
+
+use Think\Controller;
+
+class BasisController extends Controller
+{
+    public function checkLogin(){
+       if(session('?user_id')) {
+           return true;
+       }else{
+           return false;
+       }
+    }
+}
