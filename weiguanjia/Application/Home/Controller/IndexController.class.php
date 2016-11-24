0d2a2d76eccef24e627291a18ab84835f7a424d9
wzj <317619554@qq.com>
2016-11-24 10:13:39 +0800

    首页动态化和项目管理功能部分实现

--- weiguanjia/Application/Home/Controller/IndexController.class.php
+++ weiguanjia/Application/Home/Controller/IndexController.class.php
@@ -5,31 +5,41 @@ class IndexController extends Controller {
     //显示首页信息
     public function index(){
 
-        //获取轮播图
-        $img=$this->$this->shufflingImg();
-        //获取最新动态
-        //获取特色推荐
-        //获取项目展示
-        $this->assign('img',$img);
-       $this->display();
+        $shufflingImg=$this->shufflingImg();
+        $this->assign('shufflingImg',$shufflingImg);
+        $latestDynamic=$this->latestDynamic();
+        $this->assign('latestDynamic',$latestDynamic);
+        $specialRecommendation=$this->specialRecommendation();
+        $this->assign('specialRecommendation',$specialRecommendation);
+
+        $this->display();
+
     }
 
     //显示关于我们静态页面
     function aboutUs(){
-
+        $this->display();
     }
     //获取图片轮播资源
     protected function shufflingImg(){
-
+        $img=M('shuffling_img');
+        $data=$img->where(1)->field('src')->order('Id desc')->limit(3)->select();
+       return $data;
     }
 
     //获取最新动态资源
     protected function latestDynamic(){
+        $dynamic=M('dynamic');
+        $data=$dynamic->where(1)->field('title,createtime')->order('Id desc')->limit(3)->select();
+        return $data;
 
     }
 
     //获取特色推荐资源
     protected function specialRecommendation(){
+        $recommendation=M('features');
+        $data=$recommendation->where(1)->field('title,createtime')->order('Id desc')->limit(3)->select();
+        return $data;
 
     }
 
