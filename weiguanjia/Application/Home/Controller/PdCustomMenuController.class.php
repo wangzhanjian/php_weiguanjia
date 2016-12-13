3fd5393d546fd420645e57cef4caa54f0c208487
王天昊 <wangtianhao153@126.com>
2016-12-13 08:09:09 +0800

    创建自定义菜单

--- weiguanjia/Application/Home/Controller/PdCustomMenuController.class.php
+++ weiguanjia/Application/Home/Controller/PdCustomMenuController.class.php
@@ -10,21 +10,50 @@ namespace Home\Controller;
 
 
 use Think\Controller;
+use Wechat\MenuManager;
 
-class PdCustomMenuController extends Controller
+class PdCustomMenuController extends BasisController
 {
     //显示自定义菜单界面
-    public function Menu(){
-
-    }
+    public function menu(){
+        $MenuManager = new MenuManager();
+        $menu_decode = json_decode($MenuManager->ViewMenu(),true);
+        $menu = $menu_decode["menu"]["button"];
+        for ($i = 0;$i<count($menu);$i++)
+        {
+            if ($menu[$i]["sub_button"] != NULL){
+                $menu[$i]["category"] = "list";
+                $menu[$i]["type"] = "list";
+            }else{
+                $menu[$i]["category"] = "botton";
+            }
+        }
+        dump($menu);
+        $this->assign("menu",$menu);
+        $this->display();
+}
 
     //创建自定义菜单
     public function create(){
-
+        $menu_json=I();
+        $menu = htmlspecialchars_decode(json_encode($menu_json,JSON_UNESCAPED_UNICODE));
+        $MenuManager = new MenuManager();
+        $MenuManager->CreateMenu($menu);
+//        error_log($menu,3,"./log.txt");
+        dump($menu);
     }
 
     //删除自定义菜单
     public function delete(){
+        $MenuManager = new MenuManager();
+        $json = I();
+        $delete = $json["delete"];
+        $response = $MenuManager->DeleteMenu();
+        error_log($response,3,"./log.txt");
+        echo $response;
+    }
+    //获取自定义菜单
+    protected function getMenu(){
 
     }
 }
--- weiguanjia/ThinkPHP/Library/Wechat/MenuManager.class.php
+++ weiguanjia/ThinkPHP/Library/Wechat/MenuManager.class.php
@@ -10,21 +10,43 @@ use Curl\Curl;
  */
 class MenuManager extends AccessToken
 {
-    public function createMenu($menuInfo){
+    //创建菜单
+    public function CreateMenu($menuInfo){
         $accessToken=$this->getAccessToken();
         $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token={$accessToken}";
         $curl=new Curl();
         $curl->post($url,$menuInfo);
+        if ($curl->error) {
+            error_log($curl->error_message,3,"./log.txt");
+        }
+        else {
+            error_log($curl->response,3,"./log.txt");
+        }
         $curl->close();
         return true;
     }
-
-    public function viewMenu(){
+    //查看菜单
+    public function ViewMenu(){
         $accessToken=$this->getAccessToken();
         $url="https://api.weixin.qq.com/cgi-bin/menu/get?access_token={$accessToken}";
         $curl=new Curl();
         $curl->setOpt(CURLOPT_SSL_VERIFYPEER,0);
         $curl->get($url);
+        return $curl->response;
+    }
+    //删除所有菜单
+    public function DeleteMenu(){
+        $accessToken = $this->getAccessToken();
+        $url = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token={$accessToken}";
+        $curl = new Curl();
+        $curl->get($url);
+        error_log("delete",3,"./log.txt");
+        if ($curl->error) {
+            error_log($curl->error_message,3,"./log.txt");
+        }
+        else {
+            error_log($curl->response,3,"./log.txt");
+        }
         return $curl->response;
     }
 }
