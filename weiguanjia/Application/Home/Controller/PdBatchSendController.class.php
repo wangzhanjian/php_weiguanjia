f9583360de97ae7910cee619e66c64881df8ad5f
王天昊 <wangtianhao153@126.com>
2016-12-19 14:54:09 +0800

    上传群发模块

---
+++ weiguanjia/Application/Home/Controller/PdBatchSendController.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 天昊
+ * Date: 2016/12/15
+ * Time: 13:58
+ */
+
+namespace Home\Controller;
+
+
+class PdBatchSendController extends BasisController
+{
+    public function batchSend(){
+
+        $this->display();
+    }
+}
