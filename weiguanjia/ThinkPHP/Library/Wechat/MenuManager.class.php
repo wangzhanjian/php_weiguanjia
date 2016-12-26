93235433dc94d468830394a47f4e10a1dbaacf6b
王天昊 <wangtianhao153@126.com>
2016-12-26 14:10:31 +0800

    消息回复的接入模块相关类库

--- weiguanjia/ThinkPHP/Library/Wechat/MenuManager.class.php
+++ weiguanjia/ThinkPHP/Library/Wechat/MenuManager.class.php
@@ -1,7 +1,6 @@
 <?php
 namespace Wechat;
 use Curl\Curl;
-
 /**
  * Created by PhpStorm.
  * User: wzj
@@ -10,7 +9,7 @@ use Curl\Curl;
  */
 class MenuManager extends AccessToken
 {
-    //创建菜单
+    //创建菜单 ok
     public function CreateMenu($menuInfo){
         $accessToken=$this->getAccessToken();
         $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token={$accessToken}";
@@ -29,19 +28,13 @@ class MenuManager extends AccessToken
         $curl->get($url);
         return $curl->response;
     }
-    //删除所有菜单
+    //删除所有菜单 ok
     public function DeleteMenu(){
         $accessToken = $this->getAccessToken();
         $url = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token={$accessToken}";
         $curl = new Curl();
+        $curl->setOpt(CURLOPT_SSL_VERIFYPEER,0);
         $curl->get($url);
-        error_log("delete",3,"./log.txt");
-        if ($curl->error) {
-            error_log($curl->error_message,3,"./log.txt");
-        }
-        else {
-            error_log($curl->response,3,"./log.txt");
-        }
         return $curl->response;
     }
 }
