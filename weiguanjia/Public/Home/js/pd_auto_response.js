aa4c234a9cedb5bf14ada4a64bdce5bf147c6575
王天昊 <wangtianhao153@126.com>
2016-12-26 14:12:22 +0800

    自动回复设置页面相关js

--- weiguanjia/Public/Home/js/pd_auto_response.js
+++ weiguanjia/Public/Home/js/pd_auto_response.js
@@ -10,7 +10,14 @@ $(document).ready(function() {
     });
     //选择素材后界面元素更新 ok
     $('.modal-content').find('.modal-footer').find('[data-type=ensure]').click(function() {
-        $data=$('.modal-content').find('[data-selected=selected]').clone();
+        if(typeof where!="undefined"){
+            $data=$('.modal-content').find('[data-selected=selected]').clone().removeClass().addClass('col-xs-12 column');
+            if($('.modal-content').find('[data-selected=selected]').parent('.video_list').length){
+                $data.find('.data_box_video').children().removeClass().addClass('col-xs-12 column');
+            }
+        }else{
+            $data=$('.modal-content').find('[data-selected=selected]').clone();
+        }
         $data.append('<button class="btn btn-default btn-sm btn-block" style="border-radius:0;border-style:solid none none none;"><span class="glyphicon glyphicon-remove"></span> 移除</button>');
         $('.tab-pane.active').empty().append($data);
         $('.tab-pane.active').find('button').click(function(){
@@ -336,7 +343,93 @@ $(document).ready(function() {
         })
     });
 
+    //文件上传操作
+    $('#file').change(function () { //如果发生文件选择事件
+        if ($(this).val().length) {       //如果选择的文件名不为空
+            var xhr = new XMLHttpRequest();
+            var filesObj = document.getElementById('file').files[0];
+            var formData = new FormData();
+            formData.append('files', filesObj);
+            xhr.open("POST", '/Home/PdMaterial/add' + $type.toUpperCase().substr(0, 1) + $type.substr(1), true);
+            //设定下载progress事件的处理函数，该事件在数据下载时触发
+            xhr.onprogress = updateProgress;
+            //设定上传progress事件的处理函数，该事件在数据上传时触发
+            xhr.upload.onprogress = updateProgress;
+            var progressBar = document.getElementById('progressBar' + $progressWhere);
 
+            function updateProgress(event) {
+                //判断服务器是否提供数据长度信息
+                if (event.lengthComputable) {
+                    //计算得到进度数值
+                    var percentComplete = event.loaded / event.total;
+                    //在控制台记录进度数值
+                    //console.log(percentComplete);
+                    progressBar.style.width = percentComplete * 100 + '%';
+                }
+            }
+
+            xhr.send(formData);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    $result = xhr.responseText;
+                    if ($result == 'error') {
+                        $('#progressBar').css('width', '0%');
+                        alert('上传失败！');
+                    } else {
+                        $('#progressBar').css('width', '0%');
+                        $json = JSON.parse($result);
+                        $time = new Date();
+                        switch ($type) {
+                            case 'image':
+                                $ele = $('<div class="col-xs-3 column cell" data-type="data_box_cell"><div class="col-xs-12 column data_box_img"><img src="openImage?url=' + $json.url + '" height="117" width="100%" data-id="' + $json.media_id + '"/><p class="text-center">' + filesObj.name.substr(0, 15) + '</p></div></div>');
+                                $ele.css('opacity', '1.0');
+                                break;
+                            case 'voice':
+                                $ele = $('<div class="col-xs-6 column cell" data-type="data_box_cell"><div class="col-xs-12 column data_box_voice" data-id="' + $json.media_id + '"><div class="col-xs-4 column"><img src="/Home/PdMaterial/getVoiceDisplayImg" onmouseover="image_scale_animate(this,1.1)" onmouseleave="image_scale_animate(this,1.0)" onclick="voice_listen_online(this)" alt="点击播放" title="点击播放" width="100%" height="70"/> </div> <div class="col-xs-8 column"><p data-type="file_name"></p>' + filesObj.name.substr(0, 15) + '<p>' + $time.getFullYear() + '年' + ($time.getMonth() + 1) + '月' + $time.getDate() + '日' + '</p></div></div></div>');
+                                $ele.css('opacity', '1.0');
+                                break;
+                        }
+                        //为元素添加删除按钮
+                        $ele.append('<button class="btn btn-default btn-sm btn-block" style="border-radius:0;border-style:solid none none none;"><span class="glyphicon glyphicon-remove"></span> 移除</button>');
+                        $('.tab-pane.active').empty().append($ele);
+                        //为新上传的元素添加删除事件
+                        $('.tab-pane.active').find('button').click(function () {
+                            $tpl_mat = $('[data-type=sel_mat_model]').clone().css('display', 'block').attr('data-type', '');
+                            $tpl_upl = $('[data-type=upload_model]').clone().css('display', 'block').attr('data-type', '');
+                            //修正进度条和链接操作
+                            $where=$('.tab-pane.active').attr('id').substr(6);
+                            switch ($where){
+                                case 'image':
+                                    $tpl_upl.find('.progress-bar').attr('id','progressBarImage');
+                                    break;
+                                case 'voice':
+                                    $tpl_upl.find('a').text('本地上传');
+                                    $tpl_upl.find('.progress-bar').attr('id','progressBarVoice');
+                                    break;
+                                /*case 'music':
+                                 $tpl_upl.find('.progress-bar').attr('id','progressBarThumb');
+                                 break;
+                                 case 'video':
+                                 $tpl_upl.find('.progress-bar').attr('id','progressBarVideo');
+                                 break;
+                                 case 'news':
+                                 $tpl_upl.find('a').text('新建图文').attr({
+                                 'href':'/Home/PdMaterial/addNewsPage',
+                                 'target':'_blank'
+                                 });
+                                 $tpl_upl.find('.progress').hide();
+                                 break;*/
+                            }
+                            $('.tab-pane.active').empty();
+                            $('.tab-pane.active').append($tpl_mat).find('[data-toggle=modal]').attr('href', '#modal-container-' + $where);
+                            $('.tab-pane.active').append($tpl_upl);
+                        });
+                        progressBar.style.width = '0%';
+                    }
+                }
+            }
+        }
+    })
 });
 
 //获取素材 ok
@@ -364,7 +457,7 @@ function get_material_from_wechat($type,$offset) {
                 for($i=0;$i<$len;$i+=1){
                     switch($type){
                         case 'image':
-                            $ele='<div class="col-sm-3 column cell" data-type="data_box_cell" onclick="add_select_animate(this)"><div class="col-sm-12 column data_box_img"><img src="http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl='+$list.item[$i].url+'" height="117" width="100%" data-id="'+$list.item[$i].media_id+'"/><p class="text-center">'+$list.item[$i].name.substr($list.item[$i].name.lastIndexOf('/')+1,15)+'</p></div></div>';
+                            $ele='<div class="col-xs-3 column cell" data-type="data_box_cell" onclick="add_select_animate(this)"><div class="col-xs-12 column data_box_img"><img src="openImage?url='+$list.item[$i].url+'" height="117" width="100%" data-id="'+$list.item[$i].media_id+'"/><p class="text-center">'+$list.item[$i].name.substr($list.item[$i].name.lastIndexOf('/')+1,15)+'</p></div></div>';
                             $('.modal-body').find('.img_list').append($ele);
                             break;
                         case 'voice':
@@ -373,7 +466,7 @@ function get_material_from_wechat($type,$offset) {
                             $('.modal-body').find('.voice_list').append($ele);
                             break;
                         case 'thumb':
-                            $ele='<div class="col-sm-3 column cell" data-type="data_box_cell" onclick="add_select_animate(this)"><div class="col-sm-12 column data_box_img"><img src="http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl='+$list.item[$i].url+'" height="117" width="100%" data-id="'+$list.item[$i].media_id+'"/><p class="text-center">'+$list.item[$i].name.substr($list.item[$i].name.lastIndexOf('/')+1,15)+'</p></div></div>';
+                            $ele='<div class="col-xs-3 column cell" data-type="data_box_cell" onclick="add_select_animate(this)"><div class="col-xs-12 column data_box_img"><img src="openImage?url='+$list.item[$i].url+'" height="117" width="100%" data-id="'+$list.item[$i].media_id+'"/><p class="text-center">'+$list.item[$i].name.substr($list.item[$i].name.lastIndexOf('/')+1,15)+'</p></div></div>';
                             $('.modal-body').find('.img_list').append($ele);
                             break;
                         case 'video':
@@ -388,16 +481,16 @@ function get_material_from_wechat($type,$offset) {
                                 $childLen=$list.item[$i].content.news_item.length;
                                 for($k=0;$k<$childLen;$k+=1){
                                     if($k==0){
-                                        $childEle='<div class="first data_box"><img src="http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl='+auto_thumb_url($list.item[$i].content.news_item[$k].thumb_url)+'" width="100%" height="120px"> <h5 class="sm">'+$list.item[$i].content.news_item[$k].title.substr(0,15)+'</h5></div>';
+                                        $childEle='<div class="first data_box"><img src="openImage?url='+auto_thumb_url($list.item[$i].content.news_item[$k].thumb_url)+'" width="100%" height="120px"> <h5 class="sm">'+$list.item[$i].content.news_item[$k].title.substr(0,15)+'</h5></div>';
                                         $ele.find('.news_body').append($childEle);
                                     }else{
-                                        $childEle='<div class="col-xs-12 column data_box"><div class="col-xs-8 column"><h5>'+$list.item[$i].content.news_item[$k].title.substr(0,15)+'</h5></div><div class="col-xs-4 column"><img width="100%" height="70" src="http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl='+auto_thumb_url($list.item[$i].content.news_item[$k].thumb_url)+'"></div></div>'
+                                        $childEle='<div class="col-xs-12 column data_box"><div class="col-xs-8 column"><h5>'+$list.item[$i].content.news_item[$k].title.substr(0,15)+'</h5></div><div class="col-xs-4 column"><img width="100%" height="70" src="openImage?url='+auto_thumb_url($list.item[$i].content.news_item[$k].thumb_url)+'"></div></div>'
                                         $ele.find('.news_body').append($childEle);
                                     }
                                 }
                             }else{  //单图文素材
                                 $date=new Date($list.item[$i].update_time*1000);
-                                $ele='<div class="col-xs-4 column cell" onclick="add_select_animate(this)" data-type="data_box_cell"><div class="col-xs-12 column news_box"><div class="news_header_single"><h5>'+ $date.getFullYear()+'年'+$date.getMonth()+'月'+$date.getDate()+'日'+'</h5></div> <div class="col-xs-12 column news_body"> <div class="col-xs-12 column data_box"> <p>'+$list.item[$i].content.news_item[0].title.substr(0,15)+'</p><img src="http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl='+auto_thumb_url($list.item[$i].content.news_item[0].thumb_url)+'" width="100%" height="150px"><p>'+$list.item[$i].content.news_item[0].digest+'</p></div></div><div class="col-xs-12 column news_footer" data-id="'+$list.item[$i].media_id+'"></div></div></div>';
+                                $ele='<div class="col-xs-4 column cell" onclick="add_select_animate(this)" data-type="data_box_cell"><div class="col-xs-12 column news_box"><div class="news_header_single"><h5>'+ $date.getFullYear()+'年'+$date.getMonth()+'月'+$date.getDate()+'日'+'</h5></div> <div class="col-xs-12 column news_body"> <div class="col-xs-12 column data_box"> <p>'+$list.item[$i].content.news_item[0].title.substr(0,15)+'</p><img src="openImage?url='+auto_thumb_url($list.item[$i].content.news_item[0].thumb_url)+'" width="100%" height="150px"><p>'+$list.item[$i].content.news_item[0].digest+'</p></div></div><div class="col-xs-12 column news_footer" data-id="'+$list.item[$i].media_id+'"></div></div></div>';
                             }
                             $('.modal-body').find('.news_list').append($ele);
                             break;
@@ -453,92 +546,6 @@ function upload_file(ele) {
         $type='image';
     }
     $('#file').click();     //打开文件选择窗口
-    $('#file').change(function () { //如果发生文件选择事件
-        if ($(this).val().length) {       //如果选择的文件名不为空
-            var xhr = new XMLHttpRequest();
-            var filesObj = document.getElementById('file').files[0];
-            var formData = new FormData();
-            formData.append('files', filesObj);
-            xhr.open("POST", '/Home/PdMaterial/add' + $type.toUpperCase().substr(0, 1) + $type.substr(1), true);
-            //设定下载progress事件的处理函数，该事件在数据下载时触发
-            xhr.onprogress = updateProgress;
-            //设定上传progress事件的处理函数，该事件在数据上传时触发
-            xhr.upload.onprogress = updateProgress;
-            var progressBar = document.getElementById('progressBar' + $progressWhere);
-
-            function updateProgress(event) {
-                //判断服务器是否提供数据长度信息
-                if (event.lengthComputable) {
-                    //计算得到进度数值
-                    var percentComplete = event.loaded / event.total;
-                    //在控制台记录进度数值
-                    //console.log(percentComplete);
-                    progressBar.style.width = percentComplete * 100 + '%';
-                }
-            }
-
-            xhr.send(formData);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    $result = xhr.responseText;
-                    if ($result == 'error') {
-                        $('#progressBar').css('width', '0%');
-                        alert('上传失败！');
-                    } else {
-                        $('#progressBar').css('width', '0%');
-                        $json = JSON.parse($result);
-                        $time = new Date();
-                        switch ($type) {
-                            case 'image':
-                                $ele = $('<div class="col-sm-3 column cell" data-type="data_box_cell"><div class="col-sm-12 column data_box_img"><img src="http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl=' + $json.url + '" height="117" width="100%" data-id="' + $json.media_id + '"/><p class="text-center">' + filesObj.name.substr(0, 15) + '</p></div></div>');
-                                $ele.css('opacity', '1.0');
-                                break;
-                            case 'voice':
-                                $ele = $('<div class="col-xs-6 column cell" data-type="data_box_cell"><div class="col-xs-12 column data_box_voice" data-id="' + $json.media_id + '"><div class="col-xs-4 column"><img src="/Home/PdMaterial/getVoiceDisplayImg" onmouseover="image_scale_animate(this,1.1)" onmouseleave="image_scale_animate(this,1.0)" onclick="voice_listen_online(this)" alt="点击播放" title="点击播放" width="100%" height="70"/> </div> <div class="col-xs-8 column"><p data-type="file_name"></p>' + filesObj.name.substr(0, 15) + '<p>' + $time.getFullYear() + '年' + ($time.getMonth() + 1) + '月' + $time.getDate() + '日' + '</p></div></div></div>');
-                                $ele.css('opacity', '1.0');
-                                break;
-                        }
-                        //为元素添加删除按钮
-                        $ele.append('<button class="btn btn-default btn-sm btn-block" style="border-radius:0;border-style:solid none none none;"><span class="glyphicon glyphicon-remove"></span> 移除</button>');
-                        $('.tab-pane.active').empty().append($ele);
-                        //为新上传的元素添加删除事件
-                        $('.tab-pane.active').find('button').click(function () {
-                            $tpl_mat = $('[data-type=sel_mat_model]').clone().css('display', 'block').attr('data-type', '');
-                            $tpl_upl = $('[data-type=upload_model]').clone().css('display', 'block').attr('data-type', '');
-                            //修正进度条和链接操作
-                            $where=$('.tab-pane.active').attr('id').substr(6);
-                            switch ($where){
-                                case 'image':
-                                    $tpl_upl.find('.progress-bar').attr('id','progressBarImage');
-                                    break;
-                                case 'voice':
-                                    $tpl_upl.find('a').text('本地上传');
-                                    $tpl_upl.find('.progress-bar').attr('id','progressBarVoice');
-                                    break;
-                                /*case 'music':
-                                    $tpl_upl.find('.progress-bar').attr('id','progressBarThumb');
-                                    break;
-                                case 'video':
-                                    $tpl_upl.find('.progress-bar').attr('id','progressBarVideo');
-                                    break;
-                                case 'news':
-                                    $tpl_upl.find('a').text('新建图文').attr({
-                                        'href':'/Home/PdMaterial/addNewsPage',
-                                        'target':'_blank'
-                                    });
-                                    $tpl_upl.find('.progress').hide();
-                                    break;*/
-                            }
-                            $('.tab-pane.active').empty();
-                            $('.tab-pane.active').append($tpl_mat).find('[data-toggle=modal]').attr('href', '#modal-container-' + $where);
-                            $('.tab-pane.active').append($tpl_upl);
-                        });
-                        progressBar.style.width = '0%';
-                    }
-                }
-            }
-        }
-    })
 }
 
 //添加素材选择是素材框内容特效动作 ok
