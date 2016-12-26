8981c7afd53e1865c18ba27fb2b834a716e1d768
王天昊 <wangtianhao153@126.com>
2016-12-26 14:04:28 +0800

    群发模块

--- weiguanjia/Application/Home/Controller/PdBatchSendController.class.php
+++ weiguanjia/Application/Home/Controller/PdBatchSendController.class.php
@@ -11,8 +11,23 @@ namespace Home\Controller;
 
 class PdBatchSendController extends BasisController
 {
+    public function __construct()
+    {
+        parent::__construct();
+        if(!$this->isLogin()){  //用户使用该类的任何方法必须已经登录（前置条件）
+            $this->error('请先登录！','/Home/UserManager/loginPage',2);
+        }
+        if(!session(C('SESSION_APP_INFO'))){
+            $this->error('请先创建项目！','/Home/ProjectManager/centerPage',2);
+        }
+    }
     public function batchSend(){
+        $this->assignProjectCenterCommonInfo();
+        $this->display();
+    }
 
+    public function sendList(){
+        $this->assignProjectCenterCommonInfo();
         $this->display();
     }
 }
