c0ea680f6909fd479a1746c89180c53bc9eef45d
王天昊 <wangtianhao153@126.com>
2016-12-26 14:03:37 +0800

    自动回复的配置模块

--- weiguanjia/Application/Home/Controller/PdAutoResponseController.class.php
+++ weiguanjia/Application/Home/Controller/PdAutoResponseController.class.php
@@ -20,6 +20,9 @@ class PdAutoResponseController extends BasisController
         if(!$this->isLogin()){  //用户使用该类的任何方法必须已经登录（前置条件）
             $this->error('请先登录！','/Home/UserManager/loginPage',2);
         }
+        if(!session(C('SESSION_APP_INFO'))){
+            $this->error('请先创建项目！','/Home/ProjectManager/centerPage',2);
+        }
     }
 
     //显示被添加自动回复设置信息页面 ok
